fc27bd980d082e4488dd8774c767deefae3a54cf
krishna-nishant <nishantkrishna2005@gmail.com>
2025-01-29 01:22:57 +0530

    fix: update MongoDB connection string to new cluster

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const connectToDB = async () => {
     try {
-        mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.9yny4.mongodb.net/foodDB`)
+        mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nk87out.mongodb.net/foodDB`)
         console.log(`MongoDB connection successful`);
     } catch (error) {
         console.log('MongoDB connection failed')
